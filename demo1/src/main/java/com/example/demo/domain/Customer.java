6df5c31aee2538645262c4094e1081ef0aea5e84
SurenderReddySeelam <111672856+SurenderReddySeelam@users.noreply.github.com>
2024-04-21 14:06:13 -0400

    "demo commit"

---
+++ demo1/src/main/java/com/example/demo/domain/Customer.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain;
+
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+	@Id
+	private int id;
+	private String name;
+	private String age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+}
---
+++ demo1/src/main/java/com/example/demo/service/CustomerServiceInter.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.CustomerDTO;
+
+public interface CustomerServiceInter {
+	public void insertCustomer(CustomerDTO cust);
+}
